f6de180ca58c5697a419138344231c2cf6f7686c
manudokiller <una.esteban@gmail.com>
2022-09-22 21:09:56 -0600

    disease seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        dd('here');
         $this->call([
             //ImageSeeder::class,
             //IconSeeder::class,
